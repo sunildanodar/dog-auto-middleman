c42ce7a875ea2bcab7a3f07029663f16aac877fe
sunildanodar <ireachsunil@gmail.com>
2026-04-04 14:08:43 +0530

    fix: move sparkles_panel command below bot initialization to resolve NameError

--- bot.py
+++ bot.py
@@ -1,3 +1,43 @@
+import discord
+import asyncio
+import time
+import datetime
+import json
+import random
+import string
+import re
+import secrets
+import requests
+import os
+from discord.ext import commands
+from discord import ui
+from config import TOKEN, LOG_CHANNEL_ID, PROOF_CHANNEL_ID, TICKET_CATEGORY_ID, ADMIN_ID, CONFIRMATIONS_REQUIRED, BLOCKCYPHER_TOKEN, CODE_VERSION, DB_BACKUP_INTERVAL_MINUTES, REQUIRE_PERSISTENT_DB, DB_NAME, BACKUP_ALERT_MAX_AGE_MINUTES, BACKUP_STARTUP_MAX_AGE_MINUTES, PAYMENT_POLL_INTERVAL_SECONDS, LTC_NETWORK_FEE_USD, FEE_PERCENT
+from crypto import generate_ltc_wallet, generate_bep20_wallet, detect_ltc_payment, detect_usdt_payment, send_ltc, send_usdt, sweep_ltc_to_master, sweep_usdt_to_master, usd_to_ltc, decrypt_key, private_hex_to_ltc_address
+from database import init, save_ticket, update_ticket, get_ticket, get_ticket_by_channel, get_next_ticket_id, get_tickets_by_status, log_event, get_ticket_events, verify_ticket_audit_chain, create_db_backup, database_safety_snapshot, create_encrypted_backup_export
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+init()
+active_monitors = set()
+slash_synced = False
+withdraw_cooldowns = {}
+withdraw_retry_tasks = {}
+
+PAYMENT_POLL_INTERVAL_SECONDS = max(PAYMENT_POLL_INTERVAL_SECONDS, 10)
+WITHDRAW_CONFIRM_COOLDOWN_SECONDS = 180
+WITHDRAW_RETRY_BASE_SECONDS = 180
+WITHDRAW_RETRY_MAX_ATTEMPTS = 5
+SPARKLES_TITLE = "DOG AUTO MIDDLEMAN"
+SPARKLES_FOOTER = "Dog Escrow"
+SENSITIVE_COMMAND_COOLDOWN_SECONDS = 8
+MIN_DEAL_USD = 0.1
+MAX_DEAL_USD = 50000.0
+sensitive_command_last_used = {}
+withdraw_processing = set()
+fake_confirmation_tasks = {}
+payment_view_registered = False
+backup_task_started = False
+security_alert_last_sent = {}
+
 @bot.command(name='sparkles_panel', help='Show Sparkles-style panel with all options')
 async def sparkles_panel(ctx):
     embed = discord.Embed(
